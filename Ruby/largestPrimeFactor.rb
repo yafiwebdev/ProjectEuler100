5ea2ab4db2a4023332bd1f99322cd28f91646e2f
rohitgeddam <48797475+rohitgeddam@users.noreply.github.com>
2020-10-28 10:13:24 +0530

    Create largestPrimeFactor.rb
    Solves Project euler problem 3 in ruby 
    `https://projecteuler.net/problem=3`

---
+++ Ruby/largestPrimeFactor.rb
@@ -0,0 +1,21 @@
+=begin
+https://projecteuler.net/problem=3
+=end
+
+def largestPrimeFactor(n)
+    for j in 2..Math.sqrt(n)
+        if n % j == 0
+            $counter = 0
+            while n % j == 0 do
+                n = n / j
+                $counter += 1
+            end
+            puts "#{j}^#{$counter}"
+        end
+    end
+    if n != 1
+        puts "#{n}^#{1}"
+    end
+end
+
+largestPrimeFactor(600851475143)
