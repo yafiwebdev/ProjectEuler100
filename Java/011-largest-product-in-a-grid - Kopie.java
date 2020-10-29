ecb330774f04739515cf530471c3dbd70a5443c8
Jürgen Breitenbaumer <j.breitenbaumer@outlook.com>
2020-10-29 23:49:30 +0100

    corrected calculation to product instead of sum

--- Java/011-largest-product-in-a-grid - Kopie.java
+++ Java/011-largest-product-in-a-grid - Kopie.java
@@ -6,7 +6,7 @@ public class Solution{
 	static int gridSize = 20;
 	static int maxInt = 98;
 	static int[][] grid = new int[gridSize][gridSize];
-	static int maxProduct = 0;
+	static int maxSum = 0;
 	static int resultArray[][] = new int[4][2];
 	
 	public static void main(String[] args) 
@@ -14,72 +14,9 @@ public class Solution{
 		//fill grid with random numbers
         fillGrid();
 		//calculate maximum number
-		calculateMaxProductOfFourInARow();
+		calculateMaxSumOfFourInARow();
     }
 	
-
-	
-	private static void calculateMaxProductOfFourInARow(){
-		//iterate over grid from tol left to bottom right
-		for( int i = 0; i < gridSize; i++){
-			for( int j = 0; j < gridSize; j++){
-				calculateProductForNeighbor(i,j);
-			}
-		}
-		System.out.println("Max product: " + maxProduct);
-		System.out.print("Position: ");
-		for(int i = 0; i<4; i++){
-			System.out.print(resultArray[i][1] + "," + resultArray[i][0] + " ");
-		}
-	}
-	
-	private static void calculateProductForNeighbor(int row, int col){
-		//calculate horizontal if there are enough cells in row
-		if(row + 4 <= gridSize){
-			int productv = 1;
-			int[][] array = new int[4][2];
-			for(int i = 0; i<4; i++){
-				array[i][0] = row+i+1;
-				array[i][1] = col+1;
-				productv *= grid[row+i][col];
-			}
-			if(productv > maxProduct){
-				resultArray = array;
-				maxProduct = productv;
-			}
-		}
-		
-		//calculate vertical if there are enough cells in column
-		if(col + 4 <= gridSize){
-			int producth = 1;
-			int[][] array = new int[4][2];
-			for(int i = 0; i<4; i++){
-				array[i][0] = row+1;
-				array[i][1] = col+i+1;
-				producth *= grid[row][col+i];
-			}
-			if(producth > maxProduct){
-				resultArray = array;
-				maxProduct = producth;
-			}
-		}
-		
-		//caluclate vertical if there are enough cells in diagnonal
-		if(row + 4 <= gridSize && col + 4 <= gridSize){
-			int productd = 1;
-			int[][] array = new int[4][2];
-			for(int i = 0; i<4; i++){
-				array[i][0] = row+i+1;
-				array[i][1] = col+i+1;
-				productd *= grid[row+i][col+i];
-			}
-			if(productd > maxProduct){
-				resultArray = array;
-				maxProduct = productd;
-			}
-		}
-	}
-		
 	private static void fillGrid(){
 		Random rand = new Random();
 		//print column numbers
@@ -108,5 +45,67 @@ public class Solution{
 			}
 			System.out.println();
 		}
+	}
+	
+	private static void calculateMaxSumOfFourInARow(){
+		//iterate over grid from tol left to bottom right
+		for( int i = 0; i < gridSize; i++){
+			for( int j = 0; j < gridSize; j++){
+				calculateSumForNeighbor(i,j);
+			}
+		}
+		System.out.println("Max sum: " + maxSum);
+		System.out.print("Position: ");
+		for(int i = 0; i<4; i++){
+			System.out.print(resultArray[i][1] + "," + resultArray[i][0] + " ");
+		}
+	}
+	
+	private static void calculateSumForNeighbor(int row, int col){
+		//calculate horizontal if there are enough cells in row
+		if(row + 4 <= gridSize){
+			int sumv = 0;
+			int[][] array = new int[4][2];
+			for(int i = 0; i<4; i++){
+				array[i][0] = row+i+1;
+				array[i][1] = col+1;
+				sumv += grid[row+i][col];
+			}
+			if(sumv > maxSum){
+				resultArray = array;
+				maxSum = sumv;
+			}
+		}
+		
+		//calculate vertical if there are enough cells in column
+		if(col + 4 <= gridSize){
+			int sumh = 0;
+			int[][] array = new int[4][2];
+			for(int i = 0; i<4; i++){
+				array[i][0] = row+1;
+				array[i][1] = col+i+1;
+				sumh += grid[row][col+i];
+			}
+			if(sumh > maxSum){
+				resultArray = array;
+				maxSum = sumh;
+			}
+		}
+		
+		//caluclate vertical if there are enough cells in diagnonal
+		if(row + 4 <= gridSize && col + 4 <= gridSize){
+			int sumd = 0;
+			int[][] array = new int[4][2];
+			for(int i = 0; i<4; i++){
+				array[i][0] = row+i+1;
+				array[i][1] = col+i+1;
+				sumd += grid[row+i][col+i];
+			}
+			if(sumd > maxSum){
+				resultArray = array;
+				maxSum = sumd;
+			}
+		}
+		
 	}
 }
