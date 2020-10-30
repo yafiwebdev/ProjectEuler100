6984208da49cc3ab446263f17a36c58fdf2e707c
Eranda Karannagoda <ekarannagoda@gmail.com>
2020-10-30 22:51:26 +0530

    fix typ0

--- Groovy/001-multiples-of-3-and-5.groovy
+++ Groovy/001-multiples-of-3-and-5.groovy
@@ -1,7 +1,7 @@
 class Solution {
     static void main(String[] args) {
         def sum = 0
-        for(int i = 0;i<10;i++) {
+        for(int i = 0;i<1000;i++) {
             if((i % 3 == 0 || i % 5 == 0)) sum+= i
         }
         println sum
