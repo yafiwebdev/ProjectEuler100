e983b3bb899b20e9f6cadf7001d9e70a4886a571
Eranda Karannagoda <ekarannagoda@gmail.com>
2020-10-30 22:50:06 +0530

    feat: add groovy file for solve 1st problem #104

---
+++ Groovy/001-multiples-of-3-and-5.groovy
@@ -0,0 +1,9 @@
+class Solution {
+    static void main(String[] args) {
+        def sum = 0
+        for(int i = 0;i<10;i++) {
+            if((i % 3 == 0 || i % 5 == 0)) sum+= i
+        }
+        println sum
+    }
+}
