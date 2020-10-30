4edc3e0b31b3a6f0cf4405aad7c7bf74680620ac
Stephen Hogsten <stephenhogsten@gmail.com>
2020-10-30 11:49:49 -0700

    feedback updates

--- Ruby/004-largest-palindrome-product.rb
+++ Ruby/004-largest-palindrome-product.rb
@@ -5,19 +5,19 @@
 
 def is_palindrome(number) 
   number_string = number.to_s
-  l = number_string.length
-  character_matches_mirror = -> (i) { number_string[i] === number_string[l - i - 1]}
-  for i in 0...(l / 2)
+  len = number_string.length
+  character_matches_mirror = -> (i) { number_string[i] === number_string[len - i - 1]}
+  for i in 0...(len / 2)
     return false unless character_matches_mirror.(i)
   end
   true
 end
 
-def largest_palindrome_product()
+def largest_palindrome_product(top = 1000)
   best = 0
   # check each combination of two numbers 
-  for i in 100...(1000)
-    for j in i...(1000)
+  for i in 100...(top)
+    for j in i...(top)
       k = i*j
       best = k if k > best && is_palindrome(k)
     end
