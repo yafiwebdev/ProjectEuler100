dde439fe93fae6d05423c96ef8e0f76e9a7ae68a
Aryan <65497714+1611Aryan@users.noreply.github.com>
2020-10-31 01:13:44 +0530

    Create 009-Special Pythagorean triplet.js

---
+++ JavaScript/009-Special Pythagorean triplet.js
@@ -0,0 +1,8 @@
+for(let a=1;a<999;a++){
+    for(let b=1;b<999;b++){
+        for(let c=1;c<999;c++){
+            if(a+b+c==1000&&(a*a+b*b==c*c))
+               { alert(a*b*c);}
+        }
+    }
+}
