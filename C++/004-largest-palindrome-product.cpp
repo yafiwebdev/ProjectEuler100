6dc6a6534888043832cec8340b7fbec3c17b64f2
jDerp <kingforge12121@gmail.com>
2020-11-01 01:37:34 +0700

    Add C++ Solution of Problem 4

--- C++/004-largest-palindrome-product.cpp
+++ C++/004-largest-palindrome-product.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-bool checkpalindrome(int x){
-    int fwd = x, rev = 0;
+bool checkpalindrome(long long x){
+    long long fwd = x, rev = 0;
 
     //reverse digits
     while(fwd > 0){
         rev *= 10;
-        rev += fwd%10;
+        rev += fwd % 10;
         fwd /= 10;
     }
     fwd = x;
@@ -24,20 +24,21 @@ bool checkpalindrome(int x){
 }
 
 int main(){
-    int max_palindrome = 0;
+    long long max_palindrome = 0;
     int digit = 3;
-    int upper_bound = pow(10, digit) - 1;
-    int lower_bound = pow(10, digit - 1);
+    long long upper_bound = pow(10, digit) - 1;
+    long long lower_bound = pow(10, digit - 1);
 
-    //loop through all 3 digit pair
-    for(int i = upper_bound; i >= lower_bound; i--){
-        for(int j = i; j >= lower_bound; j--){
+    //loop through all 3 digit pairs
+    for(long long i = upper_bound; i >= lower_bound; i--){
+        if(i * i < max_palindrome)    break;
+        for(long long j = i; j >= lower_bound; j--){
+            if(i * j < max_palindrome)  break;
 
             //find max palindrome
-            if(checkpalindrome(i*j)){
-                max_palindrome = max(max_palindrome, i*j);
+            if(checkpalindrome(i * j)){
+                max_palindrome = max(max_palindrome, i * j);
             }
-
         }
     }
     cout << max_palindrome << endl;
